84078c15c6c6c2972ec50c611d72faef9968d4d8
Nikhil Mishra <nkmishra0103@gmail.com>
2021-01-16 00:47:06 +0530

    Added Feedback Mechanism

--- actions/actions.py
+++ actions/actions.py
@@ -7,11 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
 #
 # class ActionHelloWorld(Action):
 #
@@ -25,3 +25,20 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+class ActionFeedback(Action):
+
+    def name(self) -> Text:
+        return "actions_feedback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        message = tracker.latest_message.get('text')
+        print(message)
+
+        dispatcher.utter_message(text="Thank you for your feedback. Hope we have another chance to provide a better experience in the future.")
+
+        return []
